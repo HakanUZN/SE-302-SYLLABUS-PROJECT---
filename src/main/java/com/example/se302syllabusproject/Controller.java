e0adffaa3bc7f76ac2ef7f3f67444f976f4a55ab
egepelindag <egepelindag@gmail.com>
2023-12-03 14:57:16 +0300

    UI update
    UI has been updated. Page4 and page 5 has been done. Add, next, back buttons has been activated.

--- src/main/java/com/example/se302syllabusproject/Controller.java
+++ src/main/java/com/example/se302syllabusproject/Controller.java
@@ -71,7 +71,145 @@ public class Controller {
     @FXML
     public CheckBox TransferableSkillCourse;
 
-//PAGE1
+    @FXML
+    public CheckBox contribution11;
+    @FXML
+    public CheckBox contribution12;
+    @FXML
+    public CheckBox contribution13;
+    @FXML
+    public CheckBox contribution14;
+    @FXML
+    public CheckBox contribution15;
+    @FXML
+    public CheckBox contribution21;
+    @FXML
+    public CheckBox contribution22;
+    @FXML
+    public CheckBox contribution23;
+    @FXML
+    public CheckBox contribution24;
+    @FXML
+    public CheckBox contribution25;
+    @FXML
+    public CheckBox contribution31;
+    @FXML
+    public CheckBox contribution32;
+    @FXML
+    public CheckBox contribution33;
+
+    @FXML
+    public CheckBox contribution34;
+
+    @FXML
+    public CheckBox contribution35;
+
+    @FXML
+    public CheckBox contribution41;
+    @FXML
+    public CheckBox contribution42;
+    @FXML
+    public CheckBox contribution43;
+    @FXML
+    public CheckBox contribution44;
+    @FXML
+    public CheckBox contribution45;
+    @FXML
+    public CheckBox contribution51;
+    @FXML
+    public CheckBox contribution52;
+    @FXML
+    public CheckBox contribution53;
+    @FXML
+    public CheckBox contribution54;
+    @FXML
+    public CheckBox contribution55;
+    @FXML
+    public CheckBox contribution61;
+    @FXML
+    public CheckBox contribution62;
+    @FXML
+    public CheckBox contribution63;
+    @FXML
+    public CheckBox contribution64;
+    @FXML
+    public CheckBox contribution65;
+    @FXML
+    public CheckBox contribution71;
+    @FXML
+    public CheckBox contribution72;
+    @FXML
+    public CheckBox contribution73;
+    @FXML
+    public CheckBox contribution74;
+    @FXML
+    public CheckBox contribution75;
+    @FXML
+    public CheckBox contribution81;
+    @FXML
+    public CheckBox contribution82;
+    @FXML
+    public CheckBox contribution83;
+    @FXML
+    public CheckBox contribution84;
+    @FXML
+    public CheckBox contribution85;
+    @FXML
+    public CheckBox contribution91;
+    @FXML
+    public CheckBox contribution92;
+    @FXML
+    public CheckBox contribution93;
+    @FXML
+    public CheckBox contribution94;
+    @FXML
+    public CheckBox contribution95;
+    @FXML
+    public CheckBox contribution101;
+    @FXML
+    public CheckBox contribution102;
+    @FXML
+    public CheckBox contribution103;
+    @FXML
+    public CheckBox contribution104;
+    @FXML
+    public CheckBox contribution105;
+    @FXML
+    public CheckBox contribution111;
+    @FXML
+    public CheckBox contribution112;
+    @FXML
+    public CheckBox contribution113;
+    @FXML
+    public CheckBox contribution114;
+    @FXML
+    public CheckBox contribution115;
+    @FXML
+    public CheckBox contribution121;
+    @FXML
+    public CheckBox contribution122;
+    @FXML
+    public CheckBox contribution123;
+    @FXML
+    public CheckBox contribution124;
+    @FXML
+    public CheckBox contribution125;
+    @FXML
+    public CheckBox contribution131;
+    @FXML
+    public CheckBox contribution132;
+    @FXML
+    public CheckBox contribution133;
+    @FXML
+    public CheckBox contribution134;
+    @FXML
+    public CheckBox contribution135;
+    @FXML
+    public Button page4to5;
+
+
+
+    //PAGE1
     @FXML
     private void Page1CourseLanguageCheckBoxes(ActionEvent event) {
         CheckBox selectedCheckBox = (CheckBox) event.getSource();
@@ -271,6 +409,39 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    private void pageFourToFive(ActionEvent event) throws IOException, IOException {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Page5.fxml"));
+        Parent root = (Parent)loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void pageFourToThree(ActionEvent event) throws IOException, IOException {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Page3.fxml"));
+        Parent root = (Parent)loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    private void pageFiveToFour(ActionEvent event) throws IOException, IOException {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Page4.fxml"));
+        Parent root = (Parent)loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 
 
 }
